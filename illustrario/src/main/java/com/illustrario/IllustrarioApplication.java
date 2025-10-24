2e7f4a13e71e999d9ce44acc8940d60bcbe8f24f
kinomari <kinomarih@gmail.com>
2025-10-24 11:15:47 -0300

    página inicial, palavra-tema e layout básico

---
+++ illustrario/src/main/java/com/illustrario/IllustrarioApplication.java
@@ -0,0 +1,13 @@
+package com.illustrario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IllustrarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IllustrarioApplication.class, args);
+	}
+
+}
